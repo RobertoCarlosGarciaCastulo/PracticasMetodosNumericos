372016aa6a692657047ea8314be56313aa6e8b84
RobertoCarlosGarciaCastulo <52472433+RobertoCarlosGarciaCastulo@users.noreply.github.com>
2019-07-03 16:52:28 -0500

    Add files via upload

--- Biseccion.py
+++ Biseccion.py
@@ -1,10 +1,10 @@
 import numpy as np 
 def f(x):
-	return x - np.sin(x)
-a = -1
-b = 1
+	return x**4-10*x**3+3*x**2+x+23
+a = 9
+b = 12
 error = 0.4
-while error > 1e-8:
+while error > 1e-12:
 	c = (a + b) / 2
 	fa = f(a)
 	fb = f(b)
@@ -12,9 +12,9 @@ while error > 1e-8:
 	if fc == 0:
 		raiz = c
 	elif fa * fc < 0:
-		a = c
-	elif fb * fc < 0:
 		b = c
+	elif fb * fc < 0:
+		a = c
 	raiz = c
 	error = abs(fc)
 	
--- Newton.Rphaston.py
+++ Newton.Rphaston.py
@@ -4,11 +4,13 @@ def f(x):
 def df(x):
 	return 4 * x**3 + 30*x**2 + 6*x + 1
 a = 1
-b = 1
-for inter in range (1,3):
-	c = a - f(a) / df(a)
-	a = c
-	print("Su Interaccion es de ",b,"raiz ",a)
+b = 2
+error = ((f(a) - f(a+1))/ f(a))
+
+while error < 0.0000000:
+	a = a - f(a) / df(a)
+	c = a
+	print("Su Interaccion es de ",b,"raiz ",c)
 	b +=1
 
 
